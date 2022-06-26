1bef42ed679d80b3d3d716119d2b9826fd05f46f
qy_gopher <yqs_ml@163.com>
2022-06-26 09:49:49 +0800

    提取路由到router.go, 使结构更清晰

--- webk.go
+++ webk.go
@@ -1,36 +1,29 @@
 package webk
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // HandlerFunc 定义请求处理函数类型
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 // Engine 实现http.Handler接口
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 // New 构造webk.Engine
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	key := r.Method + "-" + r.URL.Path
-
-	if handler, ok := e.router[key]; ok {
-		handler(w, r)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
-	}
+	c := newContext(w, r)
+	e.router.handle(c)
 }
 
 func (e *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	e.router[key] = handler
+	e.router.addRoute(method, pattern, handler)
 }
 
 // GET 定义添加GET请求的方法
